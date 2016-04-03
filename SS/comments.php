601ebf88f71ba33367071f523c00995c07bd150c
Gong Lixue <724351282@qq.com>
2016-04-03 11:33:14 +0800

    Added files via upload
    Add the comment page. Use ajax to post your comment to server. And the comment will be saved in "comment.txt"

---
+++ SS/comments.php
@@ -0,0 +1,15 @@
+﻿<?php
+	$comment = $_POST['comment'];
+	$time = date('H:i, jS F Y');
+
+	$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+	$client_ip = $_SERVER['REMOTE_ADDR'];
+	
+	$comment =$time."\r\nFrom:$client_ip"."\r\n". $comment. "\r\n"."-------------\r\n";
+	$fp = fopen("$DOCUMENT_ROOT/ss/comment.txt",'a');
+	fwrite($fp,$comment);
+
+	// echo "<meta http-equiv='Content-Type'' content='text/html; charset=utf-8'>";
+	echo "留言成功！♪(^∇^*)'";
+	
+?>
